4b799bec919af30a413238a3e1c30b6a70b02faa
Matthew Blagg <matthewscottblagg@gmail.com>
2019-10-15 13:15:34 -0500

    started mod squad

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,9 +6,28 @@
 //    console.log("Mmmmmmm... that's a good" + currentCookie);
 // }
 
-const conjunction = function (firstWord, secondWord) {
-    const conjoinedWord = `${firstWord} ${secondWord}`
-    console.log(conjoinedWord)
+// const conjunction = function (firstWord, secondWord) {
+//     const conjoinedWord = `${firstWord} ${secondWord}`
+//     console.log(conjoinedWord)
+// }
+
+// conjunction("Visa", "Card")
+
+
+{
+    const ModSquad = {
+        "members": ["Pete Cochran", "Linc Hayes", "Julie Barnes", "Capt. Adam Greer", "Chief Barney Metcalf"]
+        "series": {
+            "start": "1968",
+            "end": "1973"
+        }
+    }
+
+    const HTMLRepresentation = `<h1>The Mod Squad</h1>`
+
+    ModSquad.members.forEach(member => {
+        const HTMLRepresentation += `<div>${member}</div>`
+    })
 }
 
-conjunction("Visa", "Card")
+document.querySelector(".show-info").innerHTML = HTMLRepresentation
